a2689614843d6f1691678127b29a3acd0ab73e05
habioye <78454514+habioye@users.noreply.github.com>
2024-07-03 13:34:33 -0400

    added github users component

---
+++ src/app/github-users/github-users.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-github-users',
+  templateUrl: './github-users.component.html',
+  styleUrl: './github-users.component.css'
+})
+export class GithubUsersComponent {
+
+}
