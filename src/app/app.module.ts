07c57c7f496a1be9a45c030eacbedf351f9df37a
habioye <78454514+habioye@users.noreply.github.com>
2024-07-03 14:23:54 -0400

    added the new create user component to  fix cannot find name ' create user component'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubUsersComponent } from './github-users/github-users.component';
+import { CreateUserComponent } from './create-user/create-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubUsersComponent,
+    CreateUserComponent
   ],
   imports: [
     BrowserModule,
