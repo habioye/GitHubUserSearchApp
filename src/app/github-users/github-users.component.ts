bc461c3ee9c0c9b66e089f05c088f38760d12d2d
habioye <78454514+habioye@users.noreply.github.com>
2024-07-08 07:50:43 -0400

    added the search with the data

--- src/app/github-users/github-users.component.ts
+++ src/app/github-users/github-users.component.ts
@@ -1,10 +1,25 @@
+import { UserInfoModule } from './../user-info/user-info.interface';
 import { Component } from '@angular/core';
-
+import { UserInfoService } from '../user-info/user-info.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-github-users',
   templateUrl: './github-users.component.html',
   styleUrl: './github-users.component.css'
 })
 export class GithubUsersComponent {
+  // userserv:UserInfoService;
+  //  = new UserInfoService();
+  input:string = '';
+  data:any = {};
+  constructor(private userInfoService: UserInfoService) {
 
+  }
+  search() {
+    this.userInfoService.getInformation(this.input).subscribe((data) => {
+      this.data = data;
+
+    });
+  }
 }
